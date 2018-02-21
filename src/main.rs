85c668d2862aaa0ed288654197f2252cababf15b
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2018-02-20 23:34:29 -0800

    multithreaded echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:9000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    print!("message: {}", String::from_utf8_lossy(&buffer[..]));
+
+    stream.write(&buffer).unwrap();
+    stream.flush().unwrap();
 }
