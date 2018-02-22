e059b3225cdd1e3a87b64f8f68b9ad6d6d340e28
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2018-02-21 17:59:34 -0800

    add todo comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,22 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 
+// todo 1
+// define a struct or class to hold the connection and user name
+// problem: how to define a struct or class and understand what I can or cannot do with them
+
+// todo 2
+// define a global variable (?) to hold the array or vector of above thing
+// problem: how to define a global variable, is it possible?
+
+// todo 3
+// add or remove an item to or from the array when connected or disconnected
+// problem: how to share the global variable between threads
+
+// todo 4
+// define a function to broadcast to all connections
+// problem: how to communicate to other threads
+
 fn main() {
     let listener = TcpListener::bind("localhost:9000").unwrap();
 
